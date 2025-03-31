da85fec57b0148482eb9ad81f79437f3124b096f
smit2410 <pin.smit2431@gmail.com>
2025-03-31 10:42:20 +0530

    Delete backend directory

--- backend/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getProducts } from "../controllers/productController.js"; // Ensure correct import
-
-const router = express.Router(); // Define router before using it
-
-router.get("/", getProducts);
-
-export default router; // Use default export since you're using ES modules
